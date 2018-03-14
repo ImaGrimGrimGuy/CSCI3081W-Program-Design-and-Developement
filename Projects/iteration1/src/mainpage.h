a25736db6a9d1545ca1392a16f203d6bc99dca15
Daniel Hartmann <hartm391.umn.edu>
2018-03-14 16:36:34 -0500

    fixed typo in mainpage

--- Projects/iteration1/src/mainpage.h
+++ Projects/iteration1/src/mainpage.h
@@ -8,7 +8,7 @@
  * Copyright 2018 Daniel Hartmann
  *
  * This is iteration 1 of the robot project, a simple two-dimensional video game
- * in which the user drives a robot around to capture bases while avoiding bad_guys.
+ * in which the user drives a robot around to capture bases while avoiding bad guys.
  * The graphics are all powered by nanoGUI and nanoVGL. Additionally, this is what 
  * drives the game. The game is contained within a rectangular arena, and all entities 
  * in the arena inheret from the base class ArenaEntity. The game itself allows for the
